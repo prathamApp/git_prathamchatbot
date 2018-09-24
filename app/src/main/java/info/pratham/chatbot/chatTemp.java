985e599007d052c4a3fc45395af68eacea72306d
prathamApp <prathambaner123@gmail.com>
2018-09-24 11:50:34 +0530

    ketan

--- app/src/main/java/info/pratham/chatbot/chatTemp.java
+++ app/src/main/java/info/pratham/chatbot/chatTemp.java
@@ -1,5 +1,6 @@
 package info.pratham.chatbot;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,7 @@ public class chatTemp extends AppCompatActivity {
     Button btnRight;
     @BindView(R.id.chatFlow)
     FlowLayout chatFlow;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +33,23 @@ public class chatTemp extends AppCompatActivity {
         ButterKnife.bind(this);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         chatFlow.removeAllViewsInLayout();
+        i=0;
 
     }
 
     @OnClick({R.id.btnLeft})
     public void addLeftString() {
 
-        final LinearLayout myLL = new LinearLayout(this);
+        final LinearLayout myLL;
+
+        i++;
+        myLL = findViewById(R.id.itenLeft);
+        final TextView myTextView = findViewById(R.id.textLeft);
+        myTextView.setText("Left String added  "+i);
+        myLL.addView(myTextView);
+        chatFlow.addView(myLL);
+
+/*        final LinearLayout myLL = new LinearLayout(this);
         myLL.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
 
         final TextView myTextView = new TextView(this);
@@ -45,21 +57,31 @@ public class chatTemp extends AppCompatActivity {
         myTextView.setTextSize(30);
         myTextView.setTextColor(Color.YELLOW);
         myLL.addView(myTextView);
-        chatFlow.addView(myLL);
+        chatFlow.addView(myLL);*/
 
     }
 
     @OnClick({R.id.btnRight})
     public void addRigthString() {
 
-        final LinearLayout myLL = new LinearLayout(this);
+        final LinearLayout myLL;
+
+        i++;
+
+        myLL = findViewById(R.id.itenRight);
+        final TextView myTextView = findViewById(R.id.textRight);
+        myTextView.setText("Right String added  "+i);
+        myLL.addView(myTextView);
+        chatFlow.addView(myLL);
+
+/*        final LinearLayout myLL = new LinearLayout(this);
         final TextView myTextView = new TextView(this);
         myTextView.setText("Left String added");
         myTextView.setTextSize(30);
         myTextView.setGravity(2);
         myTextView.setTextColor(Color.GREEN);
         myLL.addView(myTextView);
-        chatFlow.addView(myLL);
+        chatFlow.addView(myLL);*/
 
     }
 }
